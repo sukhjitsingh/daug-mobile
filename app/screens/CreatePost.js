93ad105acf76be80ca4514f97ddd67a685d72eb6
Sukhjit Singh <hms2012ca@gmail.com>
2018-03-25 11:00:26 -0700

    implemented api call for createpost and updated gitignore

--- app/screens/CreatePost.js
+++ app/screens/CreatePost.js
@@ -3,13 +3,92 @@ import { Platform, View, Text, StyleSheet, Image, TouchableOpacity } from 'react
 import { Button, Input } from 'react-native-elements';
 import { EvilIcons } from '@expo/vector-icons';
 
-export default class EditProfile extends Component {
+export default class CreatePost extends Component {
   static navigationOptions = {
     title: 'Create Post',
     headerStyle: { backgroundColor: '#2C7181', borderBottomWidth: 0 },
     headerTitleStyle: { fontSize: 20 },
   };
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userId: 7,
+      newPostContent: '',
+      isLoading: false
+    }
+  }
+
+
+  async createButtonPressed() {
+    this.setState({ isLoading: true })
+
+    const { userId, newPostContent, isLoading } = this.state
+    const { navigate } = this.props.navigation
+
+    var details = {};
+
+    if (newPostContent !== null && newPostContent.length > 0) {
+      details.description = newPostContent
+    }
+
+    var formBody = [];
+
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+
+    formBody = formBody.join("&");
+
+    try {
+      let response = await fetch(`https://daug-app.herokuapp.com/api/users/${userId}/posts`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: formBody
+      });
+
+      let responseJSON = null
+
+      if (response.status === 201) {
+        responseJSON = await response.json();
+
+        console.log(responseJSON)
+
+        this.setState({ isLoading: false })
+        Alert.alert(
+          'Post created!',
+          'You have successfully signed up!',
+          [
+            { text: "Continue", onPress: () => navigate("SocialFeed") }
+          ],
+          { cancelable: false }
+        )
+      } else {
+        responseJSON = await response.json();
+        const error = responseJSON.message
+
+        console.log(responseJSON)
+
+        this.setState({ isLoading: false, errors: responseJSON.errors })
+        Alert.alert('Create Post failed', `Unable to signup.. ${error}!`)
+      }
+    } catch (error) {
+      this.setState({ isLoading: false, response: error })
+
+      console.log(error)
+
+      Alert.alert('Sign up failed!', 'Unable to Signup. Please try again later')
+    }
+  }
+
   render() {
+    const { newPostContent } = this.state
     return (
       <View style={styles.container}>
         <View style={styles.bannerContainer}>
@@ -36,7 +115,20 @@ export default class EditProfile extends Component {
             autoCapitalize="none"
             autoCorrect={false}
             returnKeyType="next"
+            onChangeText={(newPostContent) => this.setState({ newPostContent })}
           />
+
+          <Button
+            text='Cancel'
+            buttonStyle={{
+              backgroundColor: "#81542C",
+              borderColor: "#81542C",
+              borderWidth: 1,
+              borderRadius: 8,
+              paddingHorizontal: 5,
+              marginTop: 10,
+            }} />
+
           <Button
             text='Share'
             buttonStyle={{
@@ -46,47 +138,48 @@ export default class EditProfile extends Component {
               borderRadius: 8,
               paddingHorizontal: 5,
               marginTop: 10,
-            }}/>
+            }}
+            onPress={() => this.createButtonPressed()} />
         </View>
-        </View>
-        );
-      }
-    }
-    
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-          container: {
-          flex: 1,
-        backgroundColor: '#FFF',
-      },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
   bannerContainer: {
-          flex: 1,
-        flexDirection: 'row',
-        paddingLeft: 10,
-        paddingTop: 10,
-      },
+    flex: 1,
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
   subBannerContainer: {
-          flex: 2,
-        paddingLeft: 10,
-        paddingTop: 10,
-      },
+    flex: 2,
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
   avatar: {
-          width: 80,
-        height: 80,
-        borderRadius: 40,
-      },
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
   inputContainer: {
-          flex: 1,
-        alignItems: 'center',
-        marginVertical: 10,
-      },
-    
+    flex: 1,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+
   shareButton: {
-          fontSize: 18, 
-        padding: 5, 
-        borderWidth: 1, 
-        borderColor: '#81542C', 
-        borderRadius: 5, 
-        marginTop: 20,
-        marginHorizontal: 10,
-      }
-    })
+    fontSize: 18,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#81542C',
+    borderRadius: 5,
+    marginTop: 20,
+    marginHorizontal: 10,
+  }
+})
